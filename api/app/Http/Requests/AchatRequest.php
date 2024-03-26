3993df2747988ecbcaa1a77697e81886bf1cb314
loic-njimeli <loicnjimeli13@gmail.com>
2024-03-26 14:18:14 +0100

    creations formes request

---
+++ api/app/Http/Requests/AchatRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AchatRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            "date"=> ["required","date"],
+            "prix_total"=> ["required", "number"],
+            "quantite"=> ["required", "number"],
+    
+
+
+        ];
+    }
+}
