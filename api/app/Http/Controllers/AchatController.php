6167be968dcb14b644e3fbe61995ffab80e2ade2
juniorledoux <juni.ledoux17@gmail.com>
2024-04-16 13:14:19 +0100

    terminer les fonctions de achatController

--- api/app/Http/Controllers/AchatController.php
+++ api/app/Http/Controllers/AchatController.php
@@ -2,13 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AchatRequest;
 use Illuminate\Http\Request;
 use App\Models\Achat;
 
 class AchatController extends Controller
 {
-    public function index(){
-        $listes_achats=Achat::All();
-        return response()->json($listes_achats,200);
+    //fonction pour recuperer la liste des achats en bd
+    public function index()
+    {
+        $listes_achats = Achat::All();
+        return response()->json($listes_achats, 200);
+    }
+
+    //fonction pour recuperer un seul achat
+    public function show(Request $request, $id)
+    {
+        $achat = Achat::findOrFail($id);
+        if (!$achat) return response()->json("Not found", 404);
+        return response()->json($achat, 200);
+    }
+
+    //fonction pour inserer un achat en bd
+    public function store(AchatRequest $request)
+    {
+        $achat = Achat::create($request->all());
+        return response()->json($achat, 201);
+    }
+
+    //fonction pour modifier un achat
+    public function update(Request $request, $id)
+    {
+        $achat = Achat::whereId($id)->update($request->all());
+        return response()->json($achat, 201);
+    }
+
+    //fonction pour supprimer un achat
+    public function delete(Request $request, $id)
+    {
+        Achat::whereId($id)->delete();
+        return response()->json("Achat supprimé avec succès", 200);
     }
 }
